25a0f3f00919656678b74a30b28964a04cf24b3e
07ReuelBodhak <reuelbodhak07@gmail.com>
2026-02-12 14:03:25 +0530

    add : session check in before approving

--- src/app/qr-login/page.jsx
+++ src/app/qr-login/page.jsx
@@ -1,7 +1,17 @@
 import { Suspense } from "react";
 import QRClient from "./QrClient";
 
+import { useSession, signIn } from "next-auth/react";
+
 export default function QRLoginPage() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <button onClick={() => signIn("discord")}>Login first to approve</button>
+    );
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-black">
       <Suspense fallback={<p className="text-white">Loading...</p>}>
